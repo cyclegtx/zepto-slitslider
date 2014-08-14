2ec2d643911d1b465ec2ae7cd5c199422c3c4396
cyclegtx <83872309@qq.com>
2014-08-14 18:04:51 +0800

    init

---
+++ js/zepto.slitslider.js
@@ -0,0 +1,144 @@
+/**
+* Zepto滑动插件，
+* 依赖js: zepto.js, zepto.fx.js
+*
+**/
+var SlitSlider = function(container, options){
+  this.options = $.extend({
+    horizontal:true, //动画移动方向。true:垂直,false:水平
+    easing: 'ease-in-out',           // the easing function
+    speed: 350,                //动画持续时间
+    scale:1,  //图片缩放大小
+    rotateZ:0,  //图片旋转角度
+    opacity:1,  //图片透明度
+    maxTrans:200 //图片最大移动距离
+  }, options)
+  this.animating = false;
+  this.wrpEl = container;
+  this.items = $(this.wrpEl).children();
+  this.itemsNum = this.items.length;
+  $(this.wrpEl).attr('data-step',0);
+}
+
+SlitSlider.prototype = {
+  next:function(){
+    if(this.animating)
+      return;
+    var self = this;
+    var itemIndex = $(this.wrpEl).attr('data-step')>>0;
+    if(itemIndex >= this.itemsNum-1)
+      return;
+    this.animating = true;
+    var item = this.items.eq(itemIndex);
+    var pW = item.width(),pH = item.height();
+    item.wrapInner('<div class="wrapInner"></div>').wrapInner('<div class="half1">');
+    var half1 = item.find('.half1');
+    half1.clone().removeClass('half1').addClass('half2').appendTo(item);
+    var half2 = item.find('.half2');
+    var transAnim = 2;
+    var isHorizontal = this.options.horizontal;
+    if(item.attr('data-orientation') == 'horizontal'){
+      isHorizontal = true;
+    }else if(item.attr('data-orientation') == 'vertical'){
+      isHorizontal = false;
+    }
+
+    if(isHorizontal){
+      //垂直
+      half1.css({width:pW,height:pH/2,'overflow':'hidden'});
+      half2.css({width:pW,height:pH/2,'overflow':'hidden'});
+      half2.find('.wrapInner').css({'margin-top':-pH/2});
+      half1.animate({'translate3d':'0,-'+this.options.maxTrans+'%,0',rotateZ:this.options.rotateZ+"deg",scale:this.options.scale,opacity:this.options.opacity},this.options.speed,this.options.easing,function(){
+        transAnim--;
+        if(transAnim === 0){
+          //已完成
+          $(self).trigger('nextOver');
+          $(self.wrpEl).attr('data-step',itemIndex+1);
+          self.animating = false;
+        }
+      });
+      half2.animate({'translate3d':'0,'+this.options.maxTrans+'%,0',rotateZ:this.options.rotateZ+"deg",scale:this.options.scale,opacity:this.options.opacity},this.options.speed,this.options.easing,function(){
+        transAnim--;
+        if(transAnim === 0){
+          //已完成
+          $(self).trigger('nextOver');
+          $(self.wrpEl).attr('data-step',itemIndex+1);
+          self.animating = false;
+        }
+      });
+    }else{
+      //水平
+      half1.css({width:pW/2,height:pH,'overflow':'hidden','position':'absolute','top':'0px','left':'0px'});
+      half2.css({width:pW/2,height:pH,'overflow':'hidden','position':'absolute','top':'0px','right':'0px'});
+      half1.find('.wrapInner').css({width:pW,height:pH});
+      half2.find('.wrapInner').css({width:pW,height:pH,'margin-left':-pW/2});
+      half1.animate({'translate3d':'-'+this.options.maxTrans+'%,0,0',rotateZ:this.options.rotateZ+"deg",scale:this.options.scale,opacity:this.options.opacity},this.options.speed,this.options.easing,function(){
+        transAnim--;
+        if(transAnim === 0){
+          //已完成
+          $(self).trigger('nextOver');
+          $(self.wrpEl).attr('data-step',itemIndex+1);
+          self.animating = false;
+        }
+      });
+      half2.animate({'translate3d':''+this.options.maxTrans+'%,0,0',rotateZ:this.options.rotateZ+"deg",scale:this.options.scale,opacity:this.options.opacity},this.options.speed,this.options.easing,function(){
+        transAnim--;
+        if(transAnim === 0){
+          //已完成
+          $(self).trigger('nextOver');
+          $(self.wrpEl).attr('data-step',itemIndex+1);
+          self.animating = false;
+        }
+      });
+    }
+    
+    
+    
+  },
+  prev:function(){
+    if(this.animating)
+      return;
+    var self = this;
+    var itemIndex = $(this.wrpEl).attr('data-step')>>0;
+    if(itemIndex == 0)
+      return;
+    this.animating = true;
+    var item = this.items.eq(itemIndex-1);
+    var half1 = item.find('.half1');
+    var half2 = item.find('.half2');
+    var transAnim = 2;
+    half1.animate({'translate3d':'0,0,0',rotateZ:"0deg",scale:'1',opacity:'1'},this.options.speed,this.options.easing,function(){
+      transAnim--;
+      if(transAnim === 0){
+        //已完成
+        $(self).trigger('prevOver');
+        $(self.wrpEl).attr('data-step',itemIndex-1);
+        half2.remove();
+        half1.find('.wrapInner').children().unwrap().unwrap();
+        self.animating = false;
+      }
+    });
+    half2.animate({'translate3d':'0,0,0',rotateZ:"0deg",scale:'1',opacity:'1'},this.options.speed,this.options.easing,function(){
+      transAnim--;
+      if(transAnim === 0){
+        //已完成
+        $(self).trigger('prevOver');
+        $(self.wrpEl).attr('data-step',itemIndex-1);
+        half2.remove();
+        half1.find('.wrapInner').children().unwrap().unwrap();
+        self.animating = false;
+      }
+    });
+  }
+}
+
+
+
+
+// zepto plugin
+;(function($) {
+  $.fn.SlitSlider = function(options) {
+    var s = new SlitSlider(this, options);
+    return s;
+  }
+})(window.Zepto)
